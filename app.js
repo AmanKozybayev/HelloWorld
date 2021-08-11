3d3c119f8516c281b9a3f609a5dc4a79f8e7d98f
Aman Kozybayev <Enphas@gmail.com>
2021-08-11 22:52:15 +0500

    Console log hello world

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log('hello world!');
+
+// class human {
+//     let head; 
+//     let body; 
+//     let arms; 
+//     let legs; 
+// }
+// class person {
+//     person(name, surename, age, address) {
+//         this.name=name;
+//         this.surename=surename;
+//         this.age=age;
+//         this.address=address;
+//     }
+//     let name;
+//     let surename; 
+//     let age; 
+//     let address;
+//     oneYearPassed() {
+//         this.age+=1;
+//     }
+// }
+// let aman = new person("Aman", "Kozybayev", 27, "aktobe");
+// aman.oneYearPassed();
+
+
+
+
+
